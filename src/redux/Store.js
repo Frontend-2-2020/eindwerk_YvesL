83f3bfee3f969ff234bc678d0b59ef905cdeb0f8
Yves Loeys <>
2020-04-20 10:52:35 -0700

    store

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,15 +1,3 @@
-// import { createStore } from "redux";
-// //import combinedReducers from "./reducers";
-// import AuthReducer from "./reducers/AuthReducer";
-// //import thunk from "redux-thunk";
-
-// const store = createStore(
-//   AuthReducer,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
-
-// export default store;
-
 import { createStore, compose, applyMiddleware } from "redux";
 import combinedReducers from "./reducers";
 
