b7e460f673ca1ba4139f34b9b35c767ccb2ad742
Yves Loeys <>
2020-05-13 13:01:58 -0700

    more Cleanup

--- src/redux/actions/RegisterActions.js
+++
@@ -1,8 +0,0 @@
-import { API } from "../../config/API";
-
-export const registerAction = (registerValues, history) => {
-  API.post("api/users", registerValues).then((response) => {
-    //Redirect naar login page
-    history.push("/login");
-  });
-};
